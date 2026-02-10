ecd29b3f50157422c95bfb217fa60233b2d50129
marymudengi <marymudengi@gmail.com>
2026-02-10 10:58:50 +0200

    Update file paths for bulk inserts in SQL script

--- scripts/bronze/pro_load_bronze.sql
+++ scripts/bronze/pro_load_bronze.sql
@@ -1,4 +1,4 @@
-SELECT name 
+            SELECT name 
 FROM sys.schemas
 WHERE name = 'bronze';
 
@@ -26,7 +26,7 @@ BEGIN
             TRUNCATE TABLE bronze.crm_cust_info;
 
             BULK INSERT bronze.crm_cust_info
-            FROM 'C:\Users\msinvula\OneDrive - Road Fund Administration\Desktop\SQL Datawarehouse Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+            FROM 'C:\Users\marym\Documents\datasets\source_crm\cust_info.csv'
             WITH (FIRSTROW =2,
             FIELDTERMINATOR =',',
             TABLOCK
@@ -39,7 +39,7 @@ BEGIN
             TRUNCATE TABLE bronze.crm_prd_info;
 
             BULK INSERT bronze.crm_prd_info
-            FROM 'C:\Users\msinvula\OneDrive - Road Fund Administration\Desktop\SQL Datawarehouse Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+            FROM 'C:\Users\marym\Documents\datasets\source_crm\prd_info.csv'
             WITH (FIRSTROW =2,
             FIELDTERMINATOR =',',
             TABLOCK
@@ -52,7 +52,7 @@ BEGIN
             TRUNCATE TABLE bronze.crm_sales_details;
 
             BULK INSERT bronze.crm_sales_details
-            FROM 'C:\Users\msinvula\OneDrive - Road Fund Administration\Desktop\SQL Datawarehouse Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+            FROM 'C:\Users\marym\Documents\datasets\source_crm\sales_details.csv'
             WITH (FIRSTROW =2,
             FIELDTERMINATOR =',',
             TABLOCK
@@ -69,7 +69,7 @@ BEGIN
             TRUNCATE TABLE bronze.erp_cust_az12;
 
             BULK INSERT bronze.erp_cust_az12
-            FROM 'C:\Users\msinvula\OneDrive - Road Fund Administration\Desktop\SQL Datawarehouse Project\sql-data-warehouse-project\datasets\source_erp\CUST_az12.csv'
+            FROM 'C:\Users\marym\Documents\datasets\source_erp\CUST_az12.csv'
             WITH ( FIRSTROW = 2,
             FIELDTERMINATOR =',',
             TABLOCK);
@@ -82,7 +82,7 @@ BEGIN
             TRUNCATE TABLE bronze.erp_loc_a101;
 
             BULK INSERT bronze.erp_loc_a101
-            FROM 'C:\Users\msinvula\OneDrive - Road Fund Administration\Desktop\SQL Datawarehouse Project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+            FROM 'C:\Users\marym\Documents\datasets\source_erp\loc_a101.csv'
             WITH (FIRSTROW = 2,
             FIELDTERMINATOR = ',',
             TABLOCK
@@ -96,7 +96,7 @@ BEGIN
             TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
             BULK INSERT bronze.erp_px_cat_g1v2
-            FROM 'C:\Users\msinvula\OneDrive - Road Fund Administration\Desktop\SQL Datawarehouse Project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+            FROM 'C:\Users\marym\Documents\datasets\source_erp\px_cat_g1v2.csv'
             WITH (FIRSTROW = 2,
             FIELDTERMINATOR = ',',
             TABLOCK
@@ -122,4 +122,3 @@ PRINT'Error Message' + CAST (ERROR_STATE() AS NVARCHAR);
 PRINT '========================================================'
 END CATCH
 END
-
